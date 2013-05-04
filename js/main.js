d2752036b111d2bebe7027db5a4e285839c1f453
Mikko Haapoja <me@mikkoh.com>
2013-05-04 00:04:09 -0400

    Added some test cases

--- js/main.js
+++ js/main.js
@@ -13,6 +13,86 @@ function( $, Parser ) {
 			var recog = new webkitSpeechRecognition();
 			var parser = new Parser();
 
+
+			console.log( 'add a class called cat' );
+			parser.parse( 'add a class called cat' );
+			console.log( '------------' );
+
+			console.log( 'create a class named dog' );
+			parser.parse( 'create a class named dog' );
+			console.log( '------------' );
+
+			console.log( 'add a class named giraffe' );
+			parser.parse( 'add a class named giraffe' );
+			console.log( '------------' );
+
+			console.log( 'add a class called zebra' );
+			parser.parse( 'add a class called zebra' );
+			console.log( '------------' );
+
+			console.log( '********* END OF SIMPLE CLASS CREATION **********' );
+
+
+
+			console.log( 'add a function called foo' );
+			parser.parse( 'add a function called foo' );
+			console.log( '------------' );
+
+			console.log( 'create a function named bar' );
+			parser.parse( 'create a function named bar' );
+			console.log( '------------' );
+
+			console.log( 'add a function named stool' );
+			parser.parse( 'add a function named stool' );
+			console.log( '------------' );
+
+			console.log( 'add a function called chair' );
+			parser.parse( 'add a function called chair' );
+			console.log( '------------' );
+
+			console.log( '********* END OF SIMPLE FUNCTION CREATION **********' );
+
+
+
+
+			console.log( 'create a class called cat with a function called wild thing' );
+			parser.parse( 'create a class called cat with a function called wild thing' );
+			console.log( '------------' );
+
+			console.log( 'create a class named cat and add a function named meow' );
+			parser.parse( 'create a class named cat and add a function named meow' );
+			console.log( '------------' );
+
+			console.log( 'add a function named meow to cat' );
+			parser.parse( 'add a function named meow to cat' );
+			console.log( '------------' );
+
+			console.log( 'add a parameter to meow to cat called thing' );
+			parser.parse( 'add a parameter to meow to cat called thing' );
+			console.log( '------------' );
+
+			console.log( 'add a parameter to thing' );
+			parser.parse( 'add a parameter to thing' );
+			console.log( '------------' );
+
+			console.log( 'add a function called meow to a class called cat with a parameter called three' );
+			parser.parse( 'add a function called meow to a class called cat with a parameter called three' );
+			console.log( '------------' );
+
+
+
+			console.log( 'create a class called cat with functions one two and three' ); 
+			parser.parse( 'create a class called cat with functions one two and three' );
+			console.log( '------------' );
+
+			console.log( 'add a function named meow to cat with parameters one two and three' );
+			parser.parse( 'add a function named meow to cat with parameters one two and three' );
+			console.log( '------------' );
+
+
+			
+
+
 			recog.onstart = function() { 
 				document.getElementById( 'btnStart' ).innerHTML =  'STOP'; 
 			}
