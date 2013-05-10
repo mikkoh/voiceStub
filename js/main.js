4befc7c414d7248d18de8491d8be224814d436cc
Mikko Haapoja <me@mikkoh.com>
2013-05-10 09:14:52 -0400

    Started working on ui for 'interactive' ui via voice

--- js/main.js
+++ js/main.js
@@ -8,109 +8,33 @@ requirejs.config({
 
 
 
-requirejs( ['lib/jquery', 'parser/parser', 'ui/uiFactory'], 
-function( $, Parser, UIFactory ) {
+requirejs( [ 'lib/jquery', 'parser/parser', 'ui/uiFactory', 'ui/uiEntry' ], 
+function( $, Parser, UIFactory, UIEntry ) {
 	$( function(){
 		if( 'webkitSpeechRecognition' in window) {
+			var container = $( 'body' );
+
 			var recog = new webkitSpeechRecognition();
 			var parser = new Parser();
-			var uiFactory = new UIFactory( $('body') );
-
-			uiFactory.addCommands( parser.parse( 'add a class called cat with a function called meow with a parameter called sound' ) );
+			var uiFactory = new UIFactory( container );
 
 
-			/*
-			uiFactory.addCommands( parser.parse( 'add a class called cat with a function called meow with a parameter called sound' ) );
-			uiFactory.addCommands( parser.parse( 'add a function called lick to a class named cat' ) );
-			uiFactory.addCommands( parser.parse( 'add a parameter called amount to lick' ) );
-			uiFactory.addCommands( parser.parse( 'add a parameter called dampness to lick' ) );
-			uiFactory.addCommands( parser.parse( 'add a class called dog with a function called woof with a parameter called sound' ) );
-			*/
+			var classEntry = new UIEntry( container, 'CREATE CLASS' );
+			var functionEntry = new UIEntry( container, 'CREATE FUNCTION' );
+			var parameterEntry = new UIEntry( container, 'ADD PARAMETER' );
 
-			/*
-			console.log( 'add a class called cat' );
-			parser.parse( 'add a class called cat' );
-			console.log( '------------' );
-
-			console.log( 'create a class named dog' );
-			parser.parse( 'create a class named dog' );
-			console.log( '------------' );
-
-			console.log( 'add a class named giraffe' );
-			parser.parse( 'add a class named giraffe' );
-			console.log( '------------' );
-
-			console.log( 'add a class called zebra' );
-			parser.parse( 'add a class called zebra' );
-			console.log( '------------' );
-
-			console.log( 'subtract a class called zebra' );
-			parser.parse( 'subtract a class called zebra' );
-			console.log( '------------' );
+			classEntry.init();
+			functionEntry.init();
+			parameterEntry.init();
 
 
-			console.log( '********* END OF SIMPLE CLASS CREATION **********' );
-
-
-
-			console.log( 'add a function called foo' );
-			parser.parse( 'add a function called foo' );
-			console.log( '------------' );
-
-			console.log( 'create a function named bar' );
-			parser.parse( 'create a function named bar' );
-			console.log( '------------' );
-
-			console.log( 'add a function named stool' );
-			parser.parse( 'add a function named stool' );
-			console.log( '------------' );
-
-			console.log( 'add a function called chair' );
-			parser.parse( 'add a function called chair' );
-			console.log( '------------' );
-
-			console.log( '********* END OF SIMPLE FUNCTION CREATION **********' );
-
-
-
-
-			console.log( 'create a class called cat with a function called wild thing' );
-			parser.parse( 'create a class called cat with a function called wild thing' );
-			console.log( '------------' );
-
-			console.log( 'create a class named cat and add a function named meow' );
-			parser.parse( 'create a class named cat and add a function named meow' );
-			console.log( '------------' );
-
-			console.log( 'add a function named meow to cat' );
-			parser.parse( 'add a function named meow to cat' );
-			console.log( '------------' );
-
-			console.log( 'delete a function named meow from cat' );
-			parser.parse( 'delete a function named meow from cat' );
-			console.log( '------------' );
-
-			console.log( 'add a parameter called meow to a function called foo' );
-			parser.parse( 'add a parameter called meow to a function called foo' );
-			console.log( '------------' );
-
-			console.log( 'add a function called meow to a class called cat with a parameter called three' );
-			parser.parse( 'add a function called meow to a class called cat with a parameter called three' );
-			console.log( '------------' );
-
-
-
-			console.log( 'create a class called cat with functions one two and three' ); 
-			parser.parse( 'create a class called cat with functions one two and three' );
-			console.log( '------------' );
-
-			console.log( 'add a function named meow to cat with parameters one two and three' );
-			parser.parse( 'add a function named meow to cat with parameters one two and three' );
-			console.log( '------------' );
-			*/
-
-
+			var txtField = $( '#txtField' );
+			txtField.keyup( function() {
+				console.log( parser.parse( txtField.val() ) );
+			});
 			
+
+
 
 
 			recog.onstart = function() { 
